1ce0be81c389120e9ab5c8bcbc3caab86dc65b4e
Matthew Baker <34011068+mharrisonbaker@users.noreply.github.com>
2024-06-04 22:57:20 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const countdown = () => {
-    const countDate = new Date('October 10, 2023 00:00:00').getTime();
+    const countDate = new Date('October 10, 2024 00:00:00').getTime();
     const now = new Date().getTime();
     const gap = countDate - now;
 
